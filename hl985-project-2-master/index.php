c60d60bfbae7141887e66a8556361b6d51e8de19
Hahnbee Lee <hl985@cornell.edu>
2020-06-12 22:12:07 -0400

    landing page

---
+++ hl985-project-2-master/index.php
@@ -0,0 +1,57 @@
+<?php
+// DO NOT REMOVE!
+include("includes/init.php");
+// DO NOT REMOVE!
+$title = "Home";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include("includes/head.php"); ?>
+
+<body>
+  <?php include("includes/header.php"); ?>
+  <div class="strip">
+
+    <div class="site">
+      <!-- Source: https://www.amazon.com/Posters-USA-Spider-Homecoming-Poster/dp/B06XXQ7RDQ -->
+      <img src="images/homecoming.jpg" alt="Homecoming">
+      <cite><a href="https://www.amazon.com/Posters-USA-Spider-Homecoming-Poster/dp/B06XXQ7RDQ">Source</a></cite>
+    </div>
+
+    <div class="site">
+    <!-- Source: https://www.imdb.com/title/tt7784604/ -->
+    <img src="images/exmachina.jpg" alt="Ex Machina">
+    <cite><a href="https://www.imdb.com/title/tt7784604/">Source</a></cite>
+    </div>
+
+    <div class="site">
+    <!-- Source: https://www.imdb.com/title/tt2798920/ -->
+    <img src="images/annihilation.jpg" alt="Annihilation">
+    <cite><a href="https://www.imdb.com/title/tt2798920/">Source</a></cite>
+    </div>
+
+    <div class="site">
+    <!-- Source: https://www.fandangonow.com/player/trailer/MMV152CA0FF50713E014BF44ABE5811884C8 -->
+    <img src="images/lalaland.jpg" alt="La La Land">
+    <cite><a href='https://www.fandangonow.com/player/trailer/MMV152CA0FF50713E014BF44ABE5811884C8'>Source</a></cite>
+    </div>
+
+    <div class="site">
+    <!-- Source: https://www.pinterest.com/pin/445012006912080956/?lp=true -->
+    <img src="images/perfectblue.jpg" alt="Perfect Blue">
+    <cite><a href="https://www.pinterest.com/pin/445012006912080956/?lp=true">Source</a></cite>
+    </div>
+
+    <div class="site">
+    <!-- Source: https://www.amazon.com/SPIDER-MAN-SPIDER-VERSE-POSTER-ORIGINAL-Advance/dp/B07GJR2ZSD -->
+    <img src="images/spiderverse.jpg" alt="spiderverse">
+    <cite><a href="https://www.amazon.com/SPIDER-MAN-SPIDER-VERSE-POSTER-ORIGINAL-Advance/dp/B07GJR2ZSD">Source</a></cite>
+    </div>
+
+  </div>
+
+  <h1 class="centered">Welcome! </h1>
+  <h2 class="centered">Hi, welcome to the website "Hahnbee's Favorite Movies"! Here you can find all of Hahnbee's favorite movies. Hope some of them end up catching your eye and you end up watching some of them!</h2>
+</body>
+</html>
