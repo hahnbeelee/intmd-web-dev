c8f8bb2a67626740c9a7cf9d7957a02d86d779a7
Hahnbee Lee <hl985@cornell.edu>
2020-06-12 22:02:51 -0400

    landing page

---
+++ hl985-project-1-master/index.php
@@ -0,0 +1,49 @@
+<?php
+  // DO NOT REMOVE!
+  include("includes/init.php");
+  // DO NOT REMOVE!
+  $title = "Home";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <title>Home</title>
+
+  <link rel="stylesheet" type="text/css" href="styles/all.css" media="all" />
+</head>
+
+<body>
+<?php include("includes/header.php"); ?>
+
+  <div class="index">
+      <img src="images/nature-18.png">
+      <figcaption class="right">Source: <cite>Hahnbee Lee</cite></figcaption>
+
+      <p class="right">
+        Hello,
+      </p>
+      <p class="right">
+        My name is Hahnbee Lee and I enjoy taking photos of things...
+      </p>
+      <p class="right">
+        Welcome to my photography website.
+      </p>
+      <p class="right">
+        Enjoy!
+      </p>
+
+      <?php
+        include("includes/top-button.php");
+        include("includes/footer.php");
+      ?>
+
+  </div>
+
+
+</body>
+
+</html>
